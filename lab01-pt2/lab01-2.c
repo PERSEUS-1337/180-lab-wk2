9b02b0a2ab3f8cd1fc5e349f844d43f92729f8bb
PERSEUS-1337 <abramillano@up.edu.ph>
2023-03-06 15:24:49 +0800

    added c-counterpart of code

--- lab01-pt2/lab01-2.c
+++ lab01-pt2/lab01-2.c
@@ -4,7 +4,6 @@
 
 // This is an attempt to optimize my python code and turn it into C code that is much faster
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -17,42 +16,58 @@
 
 /**
  * It creates a 2D array of floats
- * 
+ *
  * @param n the number of rows and columns in the matrix
- * 
+ *
  * @return A pointer to a pointer to a float.
  */
-float** createMatx(int n)
+float **createMatx(int n)
 {
-    float* values = calloc(n*n, sizeof(float));
-    float** rows = malloc(n*sizeof(float*));
-    for (int i=0; i<n; ++i)
+    float *values = calloc(n * n, sizeof(float));
+    float **rows = malloc(n * sizeof(float *));
+    for (int i = 0; i < n; ++i)
     {
-        rows[i] = values + i*n;
+        rows[i] = values + i * n;
     }
     return rows;
 }
 
 /**
  * It frees the memory allocated for the matrix.
- * 
+ *
  * @param matx The matrix to be destroyed.
  */
-void destroyMatx(float** matx)
+void destroyMatx(float **matx)
 {
     free(*matx);
     free(matx);
 }
 
+void getMinMax(int n)
+{
+    // Convert int to float
+    float var = (float)n / 100 * 100;
+
+    float min = 0, max = 10;
+
+    if (var > 1)
+        min = floor((n - 1) / 10) * 10;
+    if (var > 0)
+        max = ceilf(n / 10.0) * 10;
+
+    // printf("%f : %f\n", min, max);
+    printf("%i : %i", (int)min, (int)max);
+}
+
 /**
  * It prints a matrix of size n x n
- * 
+ *
  * @param matx The matrix to be printed
  * @param n the number of rows and columns in the matrix
  */
-void printMatx(float** matx, int n)
+void printMatx(float **matx, int n)
 {
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
@@ -64,52 +79,16 @@ void printMatx(float** matx, int n)
 
 /**
  * This function populates the matrix with random numbers.
- * 
+ *
  * @param matx the matrix to be populated
  * @param n the size of the matrix
  */
-void populateMatx(float** matx, int n)
+void populateMatx(float **matx, int n)
 {
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            if ((i%10 == 0) && (j%10 == 0))
-                matx[i][j] = (rand() %(1000));
-}
-
-void getMinMax(int n)
-{
-    int temp;
-    int min;
-    int max;
-    double res;
-
-    if (temp > 1)
-    { 
-        temp--;
-    } 
-    else 
-    { 
-        temp=1;
-    }
-
-    res = temp/10;
-    min = floor(res);
-    min = min * 10;
-
-    temp = n;
-    printf("%d\n", temp);
-
-    if (temp == 0)
-    {
-        temp = 1;
-    }
-    
-    res = temp/10;
-    printf("%f\n", res);
-    max = ceil(res);
-    max = max * 10;
-
-    printf("%i : %i\n", min, max);
+            if ((i % 10 == 0) && (j % 10 == 0))
+                matx[i][j] = (rand() % (1000));
 }
 
 // void terrain_inter(float** M, int n)
@@ -123,23 +102,31 @@ void getMinMax(int n)
 
 // }
 
-int main() {
-    int n;
-    printf("Enter: ");
-    scanf("%d", &n);  
+int main()
+{
+    int n = 0;
+    printf("Enter n divisible by 10: ");
+    int check = scanf("%d", &n);
+
+    while (!check || n <= 0 || n%10!=0)
+    {
+        printf("Wrong Input! Try again: ");
+        check = scanf("%d", &n);
+    }
 
     // +1 to include the actual 10th coordinate
-    // n++;
-
-    getMinMax(n);
+    n++;
 
     // Reinitializes the randomizer for C
     srand(time(NULL));
 
-    // float** matx = createMatx(n);
-    // populateMatx(matx, n);
+    float **matx = createMatx(n);
+    populateMatx(matx, n);
     // printMatx(matx, n);
-    // destroyMatx(matx);
+    destroyMatx(matx);
+
+    // getMinMax(n);
+
 
     return 0;
 }
