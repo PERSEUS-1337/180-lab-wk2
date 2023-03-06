02a5d01a9ccf32bc17949138fd501a8367285ff8
PERSEUS-1337 <abramillano@up.edu.ph>
2023-03-06 21:01:09 +0800

    feat: benchmark version of the program

--- lab01-pt2/lab01-2.c
+++ lab01-pt2/lab01-2.c
@@ -15,7 +15,7 @@
 // https://stackoverflow.com/questions/5201708/how-to-return-a-2d-array-from-a-function-in-c
 
 /**
- * It creates a 2D array of floats
+ * It creates a matrix of size n x n
  *
  * @param n the number of rows and columns in the matrix
  *
@@ -23,11 +23,11 @@
  */
 float **createMatx(int n)
 {
-    float *values = calloc(n * n, sizeof(float));
+    float *values = malloc(n * sizeof(float));
     float **rows = malloc(n * sizeof(float *));
     for (int i = 0; i < n; ++i)
     {
-        rows[i] = values + i * n;
+        rows[i] = malloc(sizeof(float) * n);
     }
     return rows;
 }
@@ -43,20 +43,24 @@ void destroyMatx(float **matx)
     free(matx);
 }
 
-void getMinMax(int n)
+int getMin(int n)
 {
-    // Convert int to float
-    float var = (float)n / 100 * 100;
+    int min = 0;
 
-    float min = 0, max = 10;
-
-    if (var > 1)
+    if (n > 1)
         min = floor((n - 1) / 10) * 10;
-    if (var > 0)
-        max = ceilf(n / 10.0) * 10;
 
-    // printf("%f : %f\n", min, max);
-    printf("%i : %i", (int)min, (int)max);
+    return min;
+}
+
+int getMax(int n)
+{
+    int max = 10;
+
+    if (n > 0)
+        max = ceil(n / 10.0) * 10;
+
+    return max;
 }
 
 /**
@@ -71,7 +75,7 @@ void printMatx(float **matx, int n)
     {
         for (int j = 0; j < n; j++)
         {
-            printf("%f ", matx[i][j]);
+            printf("%.3f ", matx[i][j]);
         }
         printf("\n");
     }
@@ -91,42 +95,118 @@ void populateMatx(float **matx, int n)
                 matx[i][j] = (rand() % (1000));
 }
 
-// void terrain_inter(float** M, int n)
-// {
-//     for (int i = 0; i < n; i++)
-//     {
-
-//     }
-
-//     print()
-
-// }
-
-int main()
+/**
+ * It takes a 2D array of floats, and interpolates the values in between the points that are already
+ * defined
+ *
+ * @param M The matrix of elevations
+ * @param n the size of the matrix
+ */
+void terrain_inter(float **M, int n)
 {
-    int n = 0;
-    printf("Enter n divisible by 10: ");
-    int check = scanf("%d", &n);
-
-    while (!check || n <= 0 || n%10!=0)
+    for (int i = 0; i < n; i++)
     {
-        printf("Wrong Input! Try again: ");
-        check = scanf("%d", &n);
+        int min_x = getMin(i);
+        int max_x = getMax(i);
+        for (int j = 0; j < n; j++)
+        {
+            if (!((i % 10 == 0) && (j % 10 == 0)))
+            {
+                int min_y = getMin(j);
+                int max_y = getMax(j);
+
+                int area_d = (abs(min_x - i) * abs(min_y - j));
+                int area_c = (abs(max_x - i) * abs(min_y - j));
+                int area_b = (abs(min_x - i) * abs(max_y - j));
+                int area_a = (abs(max_x - i) * abs(max_y - j));
+
+                float elev_a = M[min_x][min_y];
+                float elev_b = M[max_x][min_y];
+                float elev_c = M[min_x][max_y];
+                float elev_d = M[max_x][max_y];
+
+                float elevation = ((area_a * elev_a) + (area_b * elev_b) + (area_c * elev_c) + (area_d * elev_d)) / (float)(area_a + area_b + area_c + area_d);
+
+                M[i][j] = elevation;
+            }
+        }
     }
+}
 
-    // +1 to include the actual 10th coordinate
-    n++;
-
-    // Reinitializes the randomizer for C
+void run_benchmark(int n)
+{
     srand(time(NULL));
+    n++;
+    clock_t t;
 
     float **matx = createMatx(n);
     populateMatx(matx, n);
-    // printMatx(matx, n);
+
+    t = clock();
+    terrain_inter(matx, n);
+    t = clock() - t;
+    double time_taken = ((double)t) / CLOCKS_PER_SEC;
+
+    printf("Matrix size: %d | Time Elapsed: %.5f seconds\n", n - 1, time_taken);
+
     destroyMatx(matx);
+}
 
-    // getMinMax(n);
+int main()
+{
+    // int n = 0;
+    // printf("Enter n divisible by 10: ");
+    // int check = scanf("%d", &n);
 
+    // while (!check || n <= 0 || n % 10 != 0)
+    // {
+    //     printf("Wrong Input! Try again: ");
+    //     check = scanf("%d", &n);
+    // }
+    // // +1 to include the actual 10th coordinate
+    // n++;
+
+    // // Reinitializes the randomizer for C
+    // srand(time(NULL));
+
+    // float **matx = createMatx(n);
+    // populateMatx(matx, n);
+
+    // // The actual interpolation
+    // clock_t t;
+    // t = clock();
+    // terrain_inter(matx, n);
+    // t = clock() - t;
+
+    // double time_taken = ((double)t) / CLOCKS_PER_SEC;
+
+    // // Printing the matrix in a clean way
+    // // printMatx(matx, n);
+    // printf("\nElapsed time: %.5f seconds\n", time_taken);
+
+    // destroyMatx(matx);
+
+    // This part of the code is for the benchmarking for lab01-pt2
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Run # %d\n", i+1);
+        run_benchmark(100);
+        run_benchmark(200);
+        run_benchmark(300);
+        run_benchmark(400);
+        run_benchmark(500);
+        run_benchmark(600);
+        run_benchmark(700);
+        run_benchmark(800);
+        run_benchmark(900);
+        run_benchmark(1000);
+        run_benchmark(2000);
+        run_benchmark(3000);
+        run_benchmark(4000);
+        run_benchmark(8000);
+        run_benchmark(16000);
+        run_benchmark(20000);
+    }
 
     return 0;
 }
